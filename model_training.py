bc5fac7ec17f165fc4bbf7dd75e47e597888dc82
Ahmed-KM-sudo <kassmimohamed93@gmail.com>
2026-01-08 22:19:08 +0100

    Save local work: update training to KNN + metrics

--- model_training.py
+++ model_training.py
@@ -1,11 +1,13 @@
 
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.neighbors import  KNeighborsClassifier as KNN
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import joblib
 
 # Load the processed data
-df = pd.read_csv('processed_data.csv')
+df = pd.read_csv("C:\\projet\\Projet-\\processed_data.csv")
 
 # Separate features and target
 X = df.drop('Destination', axis=1)
@@ -15,10 +17,27 @@ y = df['Destination']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize and train the RandomForestClassifier model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+#model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+K=5
+
+model = KNN(n_neighbors=K)
+
 model.fit(X_train, y_train)
 
 # Save the trained model to a file
 joblib.dump(model, 'recommendation_model.joblib')
-
+    
 print("Model training complete. Model saved to 'recommendation_model.joblib'.")
+
+y_Pred = model.predict(X_test)
+
+print(y_Pred)
+
+print(classification_report(y_test, y_Pred))
+
+accurracy = accuracy_score(y_test, y_Pred )
+
+print("Accuracy:", accurracy)
+
+
