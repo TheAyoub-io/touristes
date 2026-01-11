952300653b30e6018ae796a12985caea8fbef257
Ahmed-KM-sudo <kassmimohamed93@gmail.com>
2026-01-11 12:37:55 +0100

    Update models and training

--- model_training.py
+++ model_training.py
@@ -12,7 +12,7 @@ import joblib
 
 # Load the processed data
 
-df = pd.read_csv("processed_data.csv")
+df = pd.read_csv("C:\App tourisme\Pfe-tourisme-\processed_data.csv")
 
 # Separate features and target
 
@@ -24,11 +24,6 @@ y = df['Destination']
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-
-import xgboost as xgb
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import LabelEncoder
 
 
 # Encode the target variable
@@ -52,16 +47,17 @@ param_grid = {
 }
 
 # Initialize the XGBClassifier
-xgb_model = xgb.XGBClassifier(random_state=42, eval_metric='mlogloss')
+#xgb_model = xgb.XGBClassifier(random_state=42, eval_metric='mlogloss')
 
 # Initialize GridSearchCV with fewer folds
-grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
-
+#grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+K = 5  # Number of neighbors for KNN
+model = KNN(n_neighbors=K)
 # Fit the grid search to the data
-grid_search.fit(X_train, y_train_encoded)
+model.fit(X_train, y_train_encoded)
 
 # Get the best model
-model = grid_search.best_estimator_
+#model = grid_search.best_estimator_
 
 # Make predictions on the test set
 
@@ -70,6 +66,7 @@ y_pred_encoded = model.predict(X_test)
 
 # Calculate and print the accuracy
 accuracy = accuracy_score(y_test_encoded, y_pred_encoded)
+
 report = classification_report(y_test_encoded, y_pred_encoded, target_names=le.classes_)
 
 # Save the performance to a file
@@ -78,15 +75,18 @@ with open('model_performance.txt', 'w') as f:
     f.write("Classification Report:\n")
     f.write(report)
 
-print(f"Accuracy: {accuracy}")
 
 print(report)
 
 
-
 # Save the trained model to a file
 
-joblib.dump(model, 'recommendation_model.joblib')
+#joblib.dump(model, 'recommendation_model.joblib')
 
-print("Model training complete. Model saved to 'recommendation_model.joblib'.")
+#print("Model training complete. Model saved to 'recommendation_model.joblib'.")
+
+print(len(y_pred_encoded))
+
+
+print("Accuracy:" , round(accuracy*100,2) , "%")
 
